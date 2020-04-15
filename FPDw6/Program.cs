2cf389c3c807914f1220865d9e286f773f8a973d
Jia Wen Foo <you@example.com>
2020-04-16 02:05:19 +0800

    w6 added

---
+++ FPDw6/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace FPDw6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Count to 5 (do...while loop)");
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i++);
+            } while (i <= 5);
+
+            Console.WriteLine("Count to 5 (while loop)");
+            int j = 0;
+            while (j <= 5)
+            {
+                Console.WriteLine(j++);
+            }
+
+            Console.WriteLine("Count to 5 (for loop)");
+            for (int a = 0; a <= 5; a++)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.WriteLine("Nested loop: Square");
+            for (int b = 0; b < 9; b++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            
+            Console.WriteLine("Nested loop: Triangle");
+            for (int b = 0; b < 9; b++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Nested loop: Flipped Triangle");
+            for (int b = 0; b < 9; b++)
+            {
+                for (int d = 0; d < (9-b); d++)
+                {
+                    Console.Write(" ");
+                }
+                for (int c = 0; c <= 9; c++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
+
+
