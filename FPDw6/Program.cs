bfb314800cd3ed454b6a09505d74c507432ff541
Jia Wen Foo <you@example.com>
2020-04-29 22:31:17 +0800

    replace i++ with 吃蘋果

--- FPDw6/Program.cs
+++ FPDw6/Program.cs
@@ -10,20 +10,22 @@ namespace FPDw6
             int i = 0;
             do
             {
-                Console.WriteLine(i++);
+                Console.WriteLine("吃蘋果");
+                i++;
             } while (i <= 5);
 
             Console.WriteLine("Count to 5 (while loop)");
             int j = 0;
             while (j <= 5)
             {
-                Console.WriteLine(j++);
+                Console.WriteLine("吃蘋果");
+                j++;
             }
 
             Console.WriteLine("Count to 5 (for loop)");
             for (int a = 0; a <= 5; a++)
             {
-                Console.WriteLine(a);
+                Console.Write("吃蘋果");
             }
 
             Console.WriteLine("Nested loop: Square");
@@ -35,11 +37,11 @@ namespace FPDw6
                 }
                 Console.WriteLine("");
             }
-            
+
             Console.WriteLine("Nested loop: Triangle");
             for (int b = 0; b < 9; b++)
             {
-                for (int c = 0; c < 9; c++)
+                for (int c = 0; c < b; c++)
                 {
                     Console.Write("*");
                 }
@@ -49,19 +51,16 @@ namespace FPDw6
             Console.WriteLine("Nested loop: Flipped Triangle");
             for (int b = 0; b < 9; b++)
             {
-                for (int d = 0; d < (9-b); d++)
+                for (int d = 0; d < (9 - b); d++)
                 {
                     Console.Write(" ");
                 }
-                for (int c = 0; c <= 9; c++)
+                for (int c = 0; c <= b; c++)
                 {
                     Console.Write("*");
                 }
                 Console.WriteLine("");
             }
-
         }
     }
 }
-
-
