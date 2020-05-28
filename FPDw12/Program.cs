5e23e940db783645ca51e57e5c3d9cf62306e1e8
Jia Wen Foo <you@example.com>
2020-05-28 15:04:39 +0800

    added w12 w13

---
+++ FPDw12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FPDw12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int a = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                a = a + i;
+            }
+            Console.WriteLine(a);
+
+
+        }
+    }
+}
---
+++ FPDw13/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace FPDw13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
